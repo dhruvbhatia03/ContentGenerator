1e7bb679551c6c12d73dae76eeef750b8188c743
Dhruv Bhatia <114271579+dhruvbhatia03@users.noreply.github.com>
2025-05-14 16:28:14 +0530

    Update page.tsx

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Settings() {
   return (
-    <div className='flex items-center justify-center h-full'>
+    <div className='flex items-center justify-center bg-gray-800'>
         <UserProfile/>
     </div>
   )
