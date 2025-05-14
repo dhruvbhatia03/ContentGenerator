e14d22b1824cbcfe8f722284b1af7c8dc13d1585
Dhruv Bhatia <114271579+dhruvbhatia03@users.noreply.github.com>
2025-05-14 16:22:29 +0530

    Update page.tsx

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -33,7 +33,8 @@ async function History() {
         return template;
     }
   return (
-    <div className='p-102222221q border rounded-lg bg-gray-900 text-white p-10'>
+    <div className='bg-gray-800 border rounded-lg h-screen'>
+    <div className='p-102222221q border rounded-lg bg-gray-900 text-white p-10 pb-20'>
         <h2 className='font-bold text-3xl'>History</h2>
         <p className='text-white'>Search your previously generate AI content</p>
         <div className='grid grid-cols-7 font-bold mt-5 py-3 px-3'>
@@ -60,6 +61,7 @@ async function History() {
         <hr/>
             </>
         ))}
+    </div>
     </div>
   )
 }
